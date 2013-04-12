c119f1a3f0977396aef8818765d4c1842a009ad5
Michael Fogleman <fogleman@gmail.com>
2013-04-11 20:43:07 -0400

    basic fog

--- fragment.glsl
+++ fragment.glsl
@@ -5,5 +5,9 @@ in vec2 fragment_uv;
 out vec4 color;
 
 void main() {
+    vec4 fog = vec4(0, 0, 0, 1);
+    float z = gl_FragCoord.z / gl_FragCoord.w;
+    float f = clamp(z / 30, 0, 1);
     color = texture(sampler, fragment_uv);
+    color = mix(color, fog, f);
 }

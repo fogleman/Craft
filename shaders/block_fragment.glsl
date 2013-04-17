f99ccb683162c5a08a1b76bd597b54af17d2617a
Michael Fogleman <fogleman@gmail.com>
2013-04-17 13:31:57 -0400

    shader improvements, consts

--- shaders/block_fragment.glsl
+++ shaders/block_fragment.glsl
@@ -4,25 +4,26 @@ uniform sampler2D sampler;
 uniform float timer;
 uniform vec3 camera;
 
-flat in vec4 fragment_position;
+flat in vec3 fragment_position;
 flat in vec3 fragment_normal;
 in vec2 fragment_uv;
 
 out vec3 color;
 
+const vec3 light_direction = normalize(vec3(1, 0, 1));
+const vec3 light_color = vec3(1);
+const vec3 ambient = vec3(0.4);
+const vec3 fog_color = vec3(0.53, 0.81, 0.92);
+
 void main() {
     color = vec3(texture(sampler, fragment_uv));
 
-    vec3 light_direction = normalize(vec3(1, 0, 1));
     float diffuse = max(0, dot(fragment_normal, light_direction));
-    vec3 ambient = vec3(0.4);
-    vec3 light_color = vec3(1);
     vec3 light = ambient + light_color * diffuse;
     color = min(color * light, vec3(1));
 
-    vec3 fog = vec3(0.53, 0.81, 0.92);
-    float z = distance(camera, vec3(fragment_position));
+    float z = distance(camera, fragment_position);
     float f = clamp(z / 192, 0, 1);
     f = pow(f, 4);
-    color = mix(color, fog, f);
+    color = mix(color, fog_color, f);
 }
--- shaders/block_vertex.glsl
+++ shaders/block_vertex.glsl
@@ -6,13 +6,13 @@ in vec4 position;
 in vec3 normal;
 in vec2 uv;
 
-flat out vec4 fragment_position;
+flat out vec3 fragment_position;
 flat out vec3 fragment_normal;
 out vec2 fragment_uv;
 
 void main() {
     gl_Position = matrix * position;
-    fragment_position = position;
+    fragment_position = vec3(position);
     fragment_normal = normal;
     fragment_uv = uv;
 }

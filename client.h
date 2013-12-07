1e3918c7c50bae62d6880133b8e8cb1ca95c142a
Michael Fogleman <fogleman@gmail.com>
2013-12-07 14:01:35 -0500

    cleanup

--- client.h
+++ client.h
@@ -1,12 +1,17 @@
+#ifndef _client_h_
+#define _client_h_
+
 #define DEFAULT_PORT 4080
 
 void client_enable();
 void client_disable();
 int get_client_enabled();
-void client_send(char *data);
-void client_chunk(int p, int q);
-void client_block(int p, int q, int x, int y, int z, int w);
-int client_recv(char *data);
 void client_connect(char *hostname, int port);
 void client_start();
 void client_stop();
+void client_send(char *data);
+int client_recv(char *data);
+void client_chunk(int p, int q);
+void client_block(int p, int q, int x, int y, int z, int w);
+
+#endif

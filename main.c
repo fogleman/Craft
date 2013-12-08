3e3b460ce8f593df3cc4527d7dcc8d7582345238
Michael Fogleman <fogleman@gmail.com>
2013-12-07 20:57:45 -0500

    added trees!

--- main.c
+++ main.c
@@ -47,7 +47,7 @@ int is_obstacle(int w) {
 }
 
 int is_transparent(int w) {
-    return w == 0 || w == 10 || is_plant(w);
+    return w == 0 || w == 10 || w == 15 || is_plant(w);
 }
 
 int is_destructable(int w) {
@@ -369,18 +369,45 @@ void make_world(Map *map, int p, int q) {
             if (dx < 0 || dz < 0 || dx >= CHUNK_SIZE || dz >= CHUNK_SIZE) {
                 w = -1;
             }
+            // sand and grass terrain
             for (int y = 0; y < h; y++) {
                 map_set(map, x, y, z, w);
             }
+            // TODO: w = -1 if outside of chunk
             if (w == 1) {
+                // grass
                 if (simplex2(-x * 0.1, z * 0.1, 4, 0.8, 2) > 0.6) {
                     map_set(map, x, h, z, 17);
                 }
+                // flowers
                 if (simplex2(x * 0.05, -z * 0.05, 4, 0.8, 2) > 0.7) {
                     int w = 18 + simplex2(x * 0.1, z * 0.1, 4, 0.8, 2) * 7;
                     map_set(map, x, h, z, w);
                 }
+                // trees
+                int ok = 1;
+                if (dx - 4 < 0 || dz - 4 < 0 ||
+                    dx + 4 >= CHUNK_SIZE || dz + 4 >= CHUNK_SIZE)
+                {
+                    ok = 0;
+                }
+                if (ok && simplex2(x, z, 6, 0.5, 2) > 0.84) {
+                    for (int y = h + 3; y < h + 8; y++) {
+                        for (int ox = -3; ox <= 3; ox++) {
+                            for (int oz = -3; oz <= 3; oz++) {
+                                int d = (ox * ox) + (oz * oz) + (y - (h + 4)) * (y - (h + 4));
+                                if (d < 11) {
+                                    map_set(map, x + ox, y, z + oz, 15);
+                                }
+                            }
+                        }
+                    }
+                    for (int y = h; y < h + 7; y++) {
+                        map_set(map, x, y, z, 5);
+                    }
+                }
             }
+            // clouds
             for (int y = 64; y < 72; y++) {
                 if (simplex3(x * 0.01, y * 0.1, z * 0.01, 8, 0.5, 2) > 0.75) {
                     map_set(map, x, y, z, 16);

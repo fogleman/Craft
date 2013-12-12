d5251e96e4513756ec04f783ac95df6384884c8f
Michael Fogleman <fogleman@gmail.com>
2013-12-12 16:11:35 -0500

    support backspace

--- main.c
+++ main.c
@@ -735,6 +735,16 @@ void on_key(GLFWwindow *window, int key, int scancode, int action, int mods) {
             }
         }
     }
+    if (key == GLFW_KEY_BACKSPACE) {
+        if (typing) {
+            int n = strlen(text);
+            if (n > 0) {
+                text[n - 1] = '\0';
+                printf("\b \b");
+                fflush(stdout);
+            }
+        }
+    }
     if (!typing) {
         if (key == GLFW_KEY_TAB) {
             flying = !flying;

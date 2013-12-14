b16c76234a92b48263cd5afca7afadea66df2c81
Mikkel Kjeldsen <commonquail@gmail.com>
2013-12-14 20:22:41 +0100

    Generalize key bind configuration.
    Do not require mapping of single characters.

--- config.c
+++ config.c
@@ -17,6 +17,40 @@
 #define CRAFT_KEY_LEFT 'A'
 #define CRAFT_KEY_RIGHT 'D'
 
+/* *alias is the entry value in the configuration file.
+ * key is the GLFW_KEY_* constant to map to *alias.
+ */
+typedef struct {
+    const char *alias;
+    int key;
+} key_map;
+
+static const key_map key_mapping[] = {
+    { "Up", GLFW_KEY_UP },
+    { "Down", GLFW_KEY_DOWN },
+    { "Left", GLFW_KEY_LEFT },
+    { "Right", GLFW_KEY_RIGHT },
+};
+
+static const size_t key_mappings = sizeof(key_mapping) / sizeof(key_mapping[0]);
+
+/* Finds the GLFW_KEY_* constant associated with *value in key_mapping above.
+ * If val_len == 1, value[0] is returned.
+ * Returns -1 if *value is not mapped.
+ * Plain linear search, so watch the size of the mapping.
+ */
+static int get_key(const char *value, size_t val_len) {
+    if (val_len == 1) {
+        return value[0];
+    }
+    for (size_t i = 0; i < key_mappings; ++i) {
+        if (strncicmp(value, key_mapping[i].alias, val_len) == 0) {
+            return key_mapping[i].key;
+        }
+    }
+    return -1;
+}
+
 static int handler(void *user, const char *section, const char *name,
         const char *value) {
     configuration *pconfig = (configuration*)user;
@@ -38,34 +72,21 @@ static int handler(void *user, const char *section, const char *name,
             handled = 1;
         }
     } else if (strncmp(section, "controls", 8) == 0) {
-        if (strncmp(name, "forward", 7) == 0) {
-            if (strncicmp(value, "up", 2) == 0) {
-                pconfig->forward = GLFW_KEY_UP;
-            } else {
-                pconfig->forward = toupper(value[0]);
+        int key = get_key(value, strlen(value));
+        if (key != -1) {
+            if (strncmp(name, "forward", 7) == 0) {
+                pconfig->forward = key;
+                handled = 1;
+            } else if (strncmp(name, "backward", 8) == 0) {
+                pconfig->backward = key;
+                handled = 1;
+            } else if (strncmp(name, "left", 4) == 0) {
+                pconfig->strafe_left = key;
+                handled = 1;
+            } else if (strncmp(name, "right", 5) == 0) {
+                pconfig->strafe_right = key;
+                handled = 1;
             }
-            handled = 1;
-        } else if (strncmp(name, "backward", 8) == 0) {
-            if (strncicmp(value, "down", 4) == 0) {
-                pconfig->backward = GLFW_KEY_DOWN;
-            } else {
-                pconfig->backward = toupper(value[0]);
-            }
-            handled = 1;
-        } else if (strncmp(name, "left", 4) == 0) {
-            if (strncicmp(value, "left", 4) == 0) {
-                pconfig->strafe_left = GLFW_KEY_LEFT;
-            } else {
-                pconfig->strafe_left = toupper(value[0]);
-            }
-            handled = 1;
-        } else if (strncmp(name, "right", 5) == 0) {
-            if (strncicmp(value, "right", 5) == 0) {
-                pconfig->strafe_right = GLFW_KEY_RIGHT;
-            } else {
-                pconfig->strafe_right = toupper(value[0]);
-            }
-            handled = 1;
         }
     }
 

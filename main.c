788966e07ff2052633a6f25af54d4b72e9e41044
F <fokoepxg@sharklasers.com>
2013-12-16 17:03:12 +0530

    Radar overlay
    Radar overlay around the crosshair, based on distance to other players
    on the server, with direction indicated by angle of other players from
    current player. Updates in realtime

--- main.c
+++ main.c
@@ -1293,6 +1293,54 @@ int main(int argc, char **argv) {
                 tx, ty, ts, text_buffer);
         }
 
+        // Render radar
+        glUseProgram(text_program);
+        glUniformMatrix4fv(text_matrix_loc, 1, GL_FALSE, matrix);
+        glUniform1i(text_sampler_loc, 1);
+        char radar_buffer[1024];
+        float ts1 = 8;
+        float tx1 = 0;
+        float ty1 = 0;
+        float cx = width / 2;
+        float cy = height / 2;
+        float distance = 0;
+        float to_deg = 180 / 3.1415926535;
+        float to_rad = 3.1415926535 / 180;
+        #define MAX_RADAR_DISTANCE 200
+
+        for(int i = 0; i < player_count; i++) {
+            float degrees = 0;
+            if(players[i].id) {
+                if(players[i].x >= x && players[i].z >= z) {
+                    degrees = 180 - atan((z - players[i].z) / (players[i].x - x)) * to_deg - rx * to_deg - 360;
+                }
+                else if(players[i].x >= x && players[i].z <= z) {
+                    degrees = 180 + atan((players[i].z - z) / (players[i].x - x)) * to_deg - rx * to_deg - 360;
+                }
+                else if(players[i].x <= x && players[i].z >= z) {
+                    degrees = atan((z - players[i].z)/(x - players[i].x)) * to_deg - rx * to_deg - 360;
+                }
+                else if(players[i].x <= x && players[i].z <= z) {
+                    degrees = atan((z - players[i].z)/(x - players[i].x)) * to_deg - rx * to_deg - 360;
+                }
+                degrees = abs(fmod(degrees, 360.0)) + 180;
+                degrees = abs(fmod(degrees, 360.0));
+                distance = sqrt(pow(z - players[i].z, 2) + pow(x - players[i].x, 2));
+
+                if(distance > 30)
+                    snprintf(radar_buffer,1024, "%i", players[i].id);
+                else
+                    snprintf(radar_buffer, 1024, "*");
+
+                tx1 = cx + (distance > MAX_RADAR_DISTANCE ? MAX_RADAR_DISTANCE: distance) * cos(degrees * to_rad);
+                ty1 = cy + (distance > MAX_RADAR_DISTANCE ? MAX_RADAR_DISTANCE: distance) * sin(degrees * to_rad);
+
+                print(
+                    text_position_loc, text_uv_loc,
+                    tx1, ty1, 8, radar_buffer);
+            }
+        }
+
         // RENDER 3-D HUD PARTS //
 
         set_matrix_item(matrix, width, height);

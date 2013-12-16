d27a5e9f970e928ccb093a95ac6ccf84a608bcab
Michael Fogleman <fogleman@gmail.com>
2013-12-16 07:57:33 -0500

    builder.py supports set operations for more complex models

--- builder.py
+++ builder.py
@@ -29,97 +29,120 @@ OFFSETS = [
     (0.5, 0.5, 0.5),
 ]
 
+def sphere(cx, cy, cz, r, fill=False, fx=False, fy=False, fz=False):
+    result = set()
+    for x in range(cx - r, cx + r + 1):
+        if fx and x != cx:
+            continue
+        for y in range(cy - r, cy + r + 1):
+            # if y < cy:
+            #     continue # top hemisphere only
+            if fy and y != cy:
+                continue
+            for z in range(cz - r, cz + r + 1):
+                if fz and z != cz:
+                    continue
+                inside = False
+                outside = fill
+                for dx, dy, dz in OFFSETS:
+                    ox, oy, oz = x + dx, y + dy, z + dz
+                    d2 = (ox - cx) ** 2 + (oy - cy) ** 2 + (oz - cz) ** 2
+                    d = d2 ** 0.5
+                    if d < r:
+                        inside = True
+                    else:
+                        outside = True
+                if inside and outside:
+                    result.add((x, y, z))
+    return result
+
+def circle_x(cx, cy, cz, r, fill=False):
+    return sphere(cx, cy, cz, r, fill, fx=True)
+
+def circle_y(cx, cy, cz, r, fill=False):
+    return sphere(cx, cy, cz, r, fill, fy=True)
+
+def circle_z(cx, cy, cz, r, fill=False):
+    return sphere(cx, cy, cz, r, fill, fz=True)
+
+def cuboid(x1, x2, y1, y2, z1, z2, fill=True):
+    result = set()
+    a = 0
+    a += x1 == x2
+    a += y1 == y2
+    a += z1 == z2
+    for x in range(x1, x2 + 1):
+        for y in range(y1, y2 + 1):
+            for z in range(z1, z2 + 1):
+                n = 0
+                n += x in (x1, x2)
+                n += y in (y1, y2)
+                n += z in (z1, z2)
+                if not fill and n <= a:
+                    continue
+                result.add((x, y, z))
+    return result
+
+def pyramid(y, x1, x2, z1, z2, fill=False):
+    result = set()
+    while x2 >= x1 and z2 >= z2:
+        result |= cuboid(x1, x2, y, y, z1, z2, fill)
+        y, x1, x2, z1, z2 = y + 1, x1 + 1, x2 - 1, z1 + 1, z2 - 1
+    return result
+
 class Client(object):
     def __init__(self, host=HOST, port=PORT):
         self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.conn.connect((host, port))
-    def set(self, x, y, z, w):
+    def set_block(self, x, y, z, w):
         self.conn.sendall('B,%d,%d,%d,%d\n' % (x, y, z, w))
-    def circle_x(self, cx, cy, cz, r, w, fill=False):
-        self.sphere(cx, cy, cz, r, w, fill, fx=True)
-    def circle_y(self, cx, cy, cz, r, w, fill=False):
-        self.sphere(cx, cy, cz, r, w, fill, fy=True)
-    def circle_z(self, cx, cy, cz, r, w, fill=False):
-        self.sphere(cx, cy, cz, r, w, fill, fz=True)
-    def sphere(self, cx, cy, cz, r, w, fill=False, fx=False, fy=False, fz = False):
-        for x in range(cx - r, cx + r + 1):
-            if fx and x != cx:
-                continue
-            for y in range(cy - r, cy + r + 1):
-                # if y < cy:
-                #     continue # top hemisphere only
-                if fy and y != cy:
-                    continue
-                for z in range(cz - r, cz + r + 1):
-                    if fz and z != cz:
-                        continue
-                    inside = False
-                    outside = fill
-                    for dx, dy, dz in OFFSETS:
-                        ox, oy, oz = x + dx, y + dy, z + dz
-                        d2 = (ox - cx) ** 2 + (oy - cy) ** 2 + (oz - cz) ** 2
-                        d = d2 ** 0.5
-                        if d < r:
-                            inside = True
-                        else:
-                            outside = True
-                    if inside and outside:
-                        self.set(x, y, z, w)
-    def cuboid(self, x1, x2, y1, y2, z1, z2, w, fill=True):
-        a = 0
-        a += x1 == x2
-        a += y1 == y2
-        a += z1 == z2
-        for x in range(x1, x2 + 1):
-            for y in range(y1, y2 + 1):
-                for z in range(z1, z2 + 1):
-                    n = 0
-                    n += x in (x1, x2)
-                    n += y in (y1, y2)
-                    n += z in (z1, z2)
-                    if not fill and n <= a:
-                        continue
-                    self.set(x, y, z, w)
-    def pyramid(self, y, x1, x2, z1, z2, w, fill=False):
-        while x2 >= x1 and z2 >= z2:
-            self.cuboid(x1, x2, y, y, z1, z2, w, fill)
-            y, x1, x2, z1, z2 = y + 1, x1 + 1, x2 - 1, z1 + 1, z2 - 1
+    def set_blocks(self, blocks, w):
+        key = lambda block: (block[1], block[0], block[2])
+        for x, y, z in sorted(blocks, key=key):
+            self.set_block(x, y, z, w)
 
 def main():
     client = Client()
-    # client.circle_y(0, 32, 0, 16, STONE, True)
-    # client.circle_y(0, 33, 0, 16, BRICK)
-    # client.cuboid(-1, 1, 1, 31, -1, 1, CEMENT)
-    # client.cuboid(-1024, 1024, 32, 32, -3, 3, STONE)
-    # client.cuboid(-3, 3, 32, 32, -1024, 1024, STONE)
-    # client.cuboid(-1024, 1024, 33, 33, -3, -3, BRICK)
-    # client.cuboid(-1024, 1024, 33, 33, 3, 3, BRICK)
-    # client.cuboid(-3, -3, 33, 33, -1024, 1024, BRICK)
-    # client.cuboid(3, 3, 33, 33, -1024, 1024, BRICK)
-    # client.sphere(0, 32, 0, 16, GLASS)
+    set_blocks = client.set_blocks
+    # set_blocks(circle_y(0, 32, 0, 16, True), STONE)
+    # set_blocks(circle_y(0, 33, 0, 16), BRICK)
+    # set_blocks(cuboid(-1, 1, 1, 31, -1, 1), CEMENT)
+    # set_blocks(cuboid(-1024, 1024, 32, 32, -3, 3), STONE)
+    # set_blocks(cuboid(-3, 3, 32, 32, -1024, 1024), STONE)
+    # set_blocks(cuboid(-1024, 1024, 33, 33, -3, -3), BRICK)
+    # set_blocks(cuboid(-1024, 1024, 33, 33, 3, 3), BRICK)
+    # set_blocks(cuboid(-3, -3, 33, 33, -1024, 1024), BRICK)
+    # set_blocks(cuboid(3, 3, 33, 33, -1024, 1024), BRICK)
+    # set_blocks(sphere(0, 32, 0, 16), GLASS)
     # for y in range(1, 32):
-    #     client.circle_y(0, y, 0, 4, CEMENT, True)
-    # client.circle_x(16, 33, 0, 3, BRICK)
-    # client.circle_x(-16, 33, 0, 3, BRICK)
-    # client.circle_z(0, 33, 16, 3, BRICK)
-    # client.circle_z(0, 33, -16, 3, BRICK)
+    #     set_blocks(circle_y(0, y, 0, 4, True), CEMENT)
+    # set_blocks(circle_x(16, 33, 0, 3), BRICK)
+    # set_blocks(circle_x(-16, 33, 0, 3), BRICK)
+    # set_blocks(circle_z(0, 33, 16, 3), BRICK)
+    # set_blocks(circle_z(0, 33, -16, 3), BRICK)
     # for x in range(0, 1024, 32):
-    #     client.cuboid(x - 1, x + 1, 31, 32, -1, 1, CEMENT)
-    #     client.cuboid(-x - 1, -x + 1, 31, 32, -1, 1, CEMENT)
-    #     client.cuboid(x, x, 1, 32, -1, 1, CEMENT)
-    #     client.cuboid(-x, -x, 1, 32, -1, 1, CEMENT)
+    #     set_blocks(cuboid(x - 1, x + 1, 31, 32, -1, 1), CEMENT)
+    #     set_blocks(cuboid(-x - 1, -x + 1, 31, 32, -1, 1), CEMENT)
+    #     set_blocks(cuboid(x, x, 1, 32, -1, 1), CEMENT)
+    #     set_blocks(cuboid(-x, -x, 1, 32, -1, 1), CEMENT)
     # for z in range(0, 1024, 32):
-    #     client.cuboid(-1, 1, 31, 32, z - 1, z + 1, CEMENT)
-    #     client.cuboid(-1, 1, 31, 32, -z - 1, -z + 1, CEMENT)
-    #     client.cuboid(-1, 1, 1, 32, z, z, CEMENT)
-    #     client.cuboid(-1, 1, 1, 32, -z, -z, CEMENT)
+    #     set_blocks(cuboid(-1, 1, 31, 32, z - 1, z + 1), CEMENT)
+    #     set_blocks(cuboid(-1, 1, 31, 32, -z - 1, -z + 1), CEMENT)
+    #     set_blocks(cuboid(-1, 1, 1, 32, z, z), CEMENT)
+    #     set_blocks(cuboid(-1, 1, 1, 32, -z, -z), CEMENT)
     # for x in range(0, 1024, 8):
-    #     client.set(x, 32, 0, CEMENT)
-    #     client.set(-x, 32, 0, CEMENT)
+    #     set_blocks(set_block(x, 32, 0), CEMENT)
+    #     set_blocks(set_block(-x, 32, 0), CEMENT)
     # for z in range(0, 1024, 8):
-    #     client.set(0, 32, z, CEMENT)
-    #     client.set(0, 32, -z, CEMENT)
-    # client.pyramid(12, 32, 32+64-1, 32, 32+64-1, COBBLE)
+    #     set_blocks(set_block(0, 32, z), CEMENT)
+    #     set_blocks(set_block(0, 32, -z), CEMENT)
+    # set_blocks(pyramid(12, 32, 32+64-1, 32, 32+64-1), COBBLE)
+    # outer = circle_y(0, 11, 0, 176 + 3, True)
+    # inner = circle_y(0, 11, 0, 176 - 3, True)
+    # set_blocks(outer - inner, STONE)
+    # a = sphere(-32, 48, -32, 24, True)
+    # b = sphere(-24, 40, -24, 24, True)
+    # set_blocks(a - b, PLANK)
 
 if __name__ == '__main__':
     main()

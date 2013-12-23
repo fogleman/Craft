6d1dd84217a5c8bb39c6513eb8484eff95b1a4d1
Michael Fogleman <fogleman@gmail.com>
2013-12-22 21:03:35 -0500

    tweaked shaders

--- shaders/block_fragment.glsl
+++ shaders/block_fragment.glsl
@@ -1,11 +1,11 @@
 #version 120
 
 uniform sampler2D sampler;
-uniform sampler2D sampler2;
+uniform sampler2D sky_sampler;
 uniform float timer;
+uniform float daylight;
 
 varying vec2 fragment_uv;
-varying float camera_distance;
 varying float fog_factor;
 varying float fog_height;
 varying float diffuse;
@@ -17,15 +17,11 @@ void main() {
     if (color == vec3(1.0, 0.0, 1.0)) {
         discard;
     }
-    vec3 fog_color = vec3(texture2D(sampler2, vec2(timer, fog_height)));
-    float p = sin(timer * 2.0 * pi - pi * 5.0 / 8.0);
-    p = clamp(p + 1.0, 0.0, 1.0);
-    vec3 light_color = vec3(p * 0.5);
-    vec3 ambient = vec3(p * 0.3 + 0.2);
-    if (color == vec3(1.0)) {
-        ambient += p / 4.0;
-    }
-    vec3 light = ambient + light_color * diffuse;
+    bool cloud = color == vec3(1.0, 1.0, 1.0);
+    vec3 fog_color = vec3(texture2D(sky_sampler, vec2(timer, fog_height)));
+    vec3 light_color = vec3(daylight * 0.6);
+    vec3 ambient = vec3(daylight * 0.2 + 0.2);
+    vec3 light = ambient + light_color * (cloud ? 1.0 - diffuse : diffuse);
     color = min(color * light, vec3(1.0));
     color = mix(color, fog_color, fog_factor);
     gl_FragColor = vec4(color, 1.0);

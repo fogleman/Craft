856a8c94802030e615fe8f19739c529f7187d680
Michael Fogleman <fogleman@gmail.com>
2013-12-25 21:33:56 -0500

    ring buffer refactor, sqlite commits on worker thread

--- ring.c
+++ ring.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 #include "ring.h"
 
 void ring_alloc(Ring *ring, int capacity) {
@@ -31,10 +32,10 @@ int ring_size(Ring *ring) {
 
 void ring_grow(Ring *ring) {
     Ring new_ring;
+    RingEntry entry;
     ring_alloc(&new_ring, ring->capacity * 2);
-    int p, q, x, y, z, w, key;
-    while (ring_get(ring, &p, &q, &x, &y, &z, &w, &key)) {
-        ring_put(&new_ring, p, q, x, y, z, w, key);
+    while (ring_get(ring, &entry)) {
+        ring_put(&new_ring, &entry);
     }
     free(ring->data);
     ring->capacity = new_ring.capacity;
@@ -43,37 +44,54 @@ void ring_grow(Ring *ring) {
     ring->data = new_ring.data;
 }
 
-void ring_put(
-    Ring *ring, int p, int q, int x, int y, int z, int w, int key)
-{
+void ring_put(Ring *ring, RingEntry *entry) {
     if (ring_full(ring)) {
         ring_grow(ring);
     }
-    RingEntry *entry = ring->data + ring->end;
-    entry->p = p;
-    entry->q = q;
-    entry->x = x;
-    entry->y = y;
-    entry->z = z;
-    entry->w = w;
-    entry->key = key;
+    RingEntry *e = ring->data + ring->end;
+    memcpy(e, entry, sizeof(RingEntry));
     ring->end = (ring->end + 1) % ring->capacity;
 }
 
-int ring_get(
-    Ring *ring, int *p, int *q, int *x, int *y, int *z, int *w, int *key)
-{
+void ring_put_block(Ring *ring, int p, int q, int x, int y, int z, int w) {
+    RingEntry entry;
+    entry.type = BLOCK;
+    entry.p = p;
+    entry.q = q;
+    entry.x = x;
+    entry.y = y;
+    entry.z = z;
+    entry.w = w;
+    ring_put(ring, &entry);
+}
+
+void ring_put_key(Ring *ring, int p, int q, int key) {
+    RingEntry entry;
+    entry.type = KEY;
+    entry.p = p;
+    entry.q = q;
+    entry.key = key;
+    ring_put(ring, &entry);
+}
+
+void ring_put_commit(Ring *ring) {
+    RingEntry entry;
+    entry.type = COMMIT;
+    ring_put(ring, &entry);
+}
+
+void ring_put_exit(Ring *ring) {
+    RingEntry entry;
+    entry.type = EXIT;
+    ring_put(ring, &entry);
+}
+
+int ring_get(Ring *ring, RingEntry *entry) {
     if (ring_empty(ring)) {
         return 0;
     }
-    RingEntry *entry = ring->data + ring->start;
-    *p = entry->p;
-    *q = entry->q;
-    *x = entry->x;
-    *y = entry->y;
-    *z = entry->z;
-    *w = entry->w;
-    *key = entry->key;
+    RingEntry *e = ring->data + ring->start;
+    memcpy(entry, e, sizeof(RingEntry));
     ring->start = (ring->start + 1) % ring->capacity;
     return 1;
 }

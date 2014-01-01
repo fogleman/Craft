640acce23310dd99f44044f400d1ff17afa48959
Michael Fogleman <fogleman@gmail.com>
2014-01-01 13:21:11 -0500

    signs on top faces done

--- src/main.c
+++ src/main.c
@@ -727,42 +727,54 @@ void occlusion(char neighbors[27], float result[6][4]) {
 int _gen_sign_buffer(
     GLfloat *data, float x, float y, float z, int face, const char *text)
 {
-    static const int face_dx[8] = {0, 0, -1, 1, 1, 0, -1, 0};
-    static const int face_dz[8] = {1, -1, 0, 0, 0, -1, 0, 1};
+    static const int glyph_dx[8] = {0, 0, -1, 1, 1, 0, -1, 0};
+    static const int glyph_dz[8] = {1, -1, 0, 0, 0, -1, 0, 1};
+    static const int line_dx[8] = {0, 0, 0, 0, 0, 1, 0, -1};
+    static const int line_dy[8] = {-1, -1, -1, -1, 0, 0, 0, 0};
+    static const int line_dz[8] = {0, 0, 0, 0, 1, 0, -1, 0};
     int count = 0;
     float max_width = 64;
+    float line_height = 1.25;
     char lines[1024];
     int rows = wrap(text, max_width, lines, 1024);
     rows = MIN(rows, 5);
-    int dx = face_dx[face];
-    int dz = face_dz[face];
+    int dx = glyph_dx[face];
+    int dz = glyph_dz[face];
+    int ldx = line_dx[face];
+    int ldy = line_dy[face];
+    int ldz = line_dz[face];
     float n = 1.0 / (max_width / 10);
-    float ry = y + n * (rows - 1) * 0.625;
+    float sx = x - n * (rows - 1) * (line_height / 2) * ldx;
+    float sy = y - n * (rows - 1) * (line_height / 2) * ldy;
+    float sz = z - n * (rows - 1) * (line_height / 2) * ldz;
     char *key;
     char *line = tokenize(lines, "\n", &key);
     while (line) {
         int length = strlen(line);
         int line_width = string_width(line);
         line_width = MIN(line_width, max_width);
-        float rx = x - dx * line_width / max_width / 2;
-        float rz = z - dz * line_width / max_width / 2;
+        float rx = sx - dx * line_width / max_width / 2;
+        float ry = sy;
+        float rz = sz - dz * line_width / max_width / 2;
         for (int i = 0; i < length; i++) {
-            int glyph_width = char_width(line[i]);
-            line_width -= glyph_width;
+            int width = char_width(line[i]);
+            line_width -= width;
             if (line_width < 0) {
                 break;
             }
-            rx += dx * glyph_width / max_width / 2;
-            rz += dz * glyph_width / max_width / 2;
+            rx += dx * width / max_width / 2;
+            rz += dz * width / max_width / 2;
             if (line[i] != ' ') {
                 make_character_3d(
                     data + count * 30, rx, ry, rz, n / 2, n, face, line[i]);
                 count++;
             }
-            rx += dx * glyph_width / max_width / 2;
-            rz += dz * glyph_width / max_width / 2;
+            rx += dx * width / max_width / 2;
+            rz += dz * width / max_width / 2;
         }
-        ry -= n * 1.25;
+        sx += n * line_height * ldx;
+        sy += n * line_height * ldy;
+        sz += n * line_height * ldz;
         line = tokenize(NULL, "\n", &key);
         rows--;
         if (rows <= 0) {

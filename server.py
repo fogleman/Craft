7c121aa7138a6c8ba62ab56c138c9e7d8e26a957
Michael Fogleman <fogleman@gmail.com>
2014-01-11 19:51:03 -0500

    fix

--- server.py
+++ server.py
@@ -561,7 +561,7 @@ def cleanup():
                 print delete_query % (x, y, z)
     conn.close()
     print 'commit;'
-    print sys.stderr, '%d of %d blocks will be cleaned up' % (count, total)
+    print >> sys.stderr, '%d of %d blocks will be cleaned up' % (count, total)
 
 def main():
     if len(sys.argv) == 2 and sys.argv[1] == 'cleanup':

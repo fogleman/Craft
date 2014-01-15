a75c04439192169794b577278f0df5e785ae7632
Michael Fogleman <fogleman@gmail.com>
2014-01-14 22:52:09 -0500

    added simplex functions to world.py

--- world.py
+++ world.py
@@ -1,7 +1,7 @@
 # gcc -std=c99 -O3 -shared -o world \
 #   -I src -I deps/noise deps/noise/noise.c src/world.c
 
-from ctypes import CDLL, CFUNCTYPE, c_int, c_void_p
+from ctypes import CDLL, CFUNCTYPE, c_float, c_int, c_void_p
 from collections import OrderedDict
 
 dll = CDLL('./world')
@@ -17,6 +17,16 @@ def dll_create_world(p, q):
         result[(x, y, z)] = w
     dll.create_world(p, q, WORLD_FUNC(world_func), None)
     return result
+
+dll.simplex2.restype = c_float
+dll.simplex2.argtypes = [c_float, c_float, c_int, c_float, c_float]
+def dll_simplex2(x, y, octaves=1, persistence=0.5, lacunarity=2.0):
+    return dll.simplex2(x, y, octaves, persistence, lacunarity)
+
+dll.simplex3.restype = c_float
+dll.simplex3.argtypes = [c_float, c_float, c_float, c_int, c_float, c_float]
+def dll_simplex3(x, y, z, octaves=1, persistence=0.5, lacunarity=2.0):
+    return dll.simplex3(x, y, z, octaves, persistence, lacunarity)
 
 class World(object):
     def __init__(self, seed=None, cache_size=64):

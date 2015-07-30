daf38eeac2acc1064af368fd99de1b0dfc4a8e92
Stefan Berggren <nsg@nsg.cc>
2015-07-30 17:53:07 +0200

    Add the new shader params added in PR #61
    Ref commit b111dadab72974aaac242f026beb218b55b04df4

--- src/inventory.c
+++ src/inventory.c
@@ -17,6 +17,10 @@ void render_inventory_block(Attrib *attrib, int w, float s, float x, float y, in
     glUniform3f(attrib->camera, 0, 0, 5);
     glUniform1i(attrib->sampler, 0); // GL_TEXTURE0
     glUniform1f(attrib->timer, PI*2);
+    glUniform4f(attrib->extra5,0.0, 0.0, 0.0, 0.0);
+    float identity[16];
+    mat_identity(identity);
+    glUniformMatrix4fv(attrib->extra6, 1, GL_FALSE, identity);
     float matrix[16];
     GLuint buffer;
     set_matrix_item_offs(matrix, g->width, g->height, s, x, y, sel);

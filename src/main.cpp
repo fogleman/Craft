414ac2e68b164c12e7c91e9bda1d2d385ad7cd88
Stefan Berggren <nsg@nsg.cc>
2015-10-18 17:48:14 +0200

    An empty gray window.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
-int old_main(int argc, char **argv);
+#include <nanogui/nanogui.h>
+#include <iostream>
+
+class Konstructs : public nanogui::Screen {
+public:
+    Konstructs() : nanogui::Screen(Eigen::Vector2i(1024, 768), "Konstructs") {
+        using namespace nanogui;
+    }
+};
 
 int main(int argc, char **argv) {
-    old_main(argc, argv);
+    nanogui::init();
+
+    {
+        nanogui::ref<Konstructs> app = new Konstructs();
+        app->drawAll();
+        app->setVisible(true);
+        nanogui::mainloop();
+    }
+
+    nanogui::shutdown();
     return 0;
 }

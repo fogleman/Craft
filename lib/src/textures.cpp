ec93092794eef2ad807e3bdc0306f60293d3f48a
Stefan Berggren <nsg@nsg.cc>
2016-09-10 23:22:14 +0200

    Detect if we are inside a snap and set corr paths.

--- lib/src/textures.cpp
+++ lib/src/textures.cpp
@@ -1,6 +1,8 @@
 #include <fstream>
 #include <streambuf>
 #include <gl_includes.h>
+#include <iostream>
+#include <cstdlib>
 #include "textures.h"
 #include "util.h"
 #define KONSTRUCTS_PATH_SIZE 256
@@ -15,6 +17,12 @@ namespace konstructs {
 
         if (!file_exist(path)) {
             snprintf(path, max_len, "/usr/local/share/konstructs-client/%s/%s", type, name);
+        }
+
+        if (!file_exist(path)) {
+            if(const char* env_p = std::getenv("SNAP")) {
+                snprintf(path, max_len, "%s/%s/%s", env_p, type, name);
+            }
         }
 
         if (!file_exist(path)) {

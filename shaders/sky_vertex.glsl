d6869aa43ffbf0f706587d6ceac3e2fa2d23071b
Satoshi N. M <snmatsutake@yahoo.co.jp>
2017-04-01 23:05:10 -0700

    Specify float precision required for WebGL

--- shaders/sky_vertex.glsl
+++ shaders/sky_vertex.glsl
@@ -1,4 +1,6 @@
-
+#ifdef GL_ES
+precision mediump float;
+#endif
 
 uniform mat4 matrix;
 
--- shaders/text_fragment.glsl
+++ shaders/text_fragment.glsl
@@ -1,4 +1,6 @@
-
+#ifdef GL_ES
+precision mediump float;
+#endif
 
 uniform sampler2D sampler;
 uniform bool is_sign;

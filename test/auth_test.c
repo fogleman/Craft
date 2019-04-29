6fc43da88316f8947c247f85e73c47455cb35f78
dakota-osu <altond@oregonstate.edu>
2019-04-28 18:24:34 -0700

    adds tests for getting access token

--- test/auth_test.c
+++ test/auth_test.c
@@ -11,6 +11,8 @@
 char* test_user_name 	  = "we-dem-boys";
 char* test_identity_token = "e8502b7afe8c408d9621474f33905d9d"; 
 
+char* fake_user_name = "this name is fake";
+char* fake_identity_token = "this id token is fake";
 
 static void handles_valid_user() {
 	char buffer[2048];
@@ -21,31 +23,42 @@ static void handles_valid_user() {
 } 
 
 static void handles_invalid_user() {
-	CU_PASS("Implement Later");
+	char buffer[2048];
+
+    int result_code = get_access_token(buffer, strlen(fake_user_name), fake_user_name, fake_identity_token);
+
+    CU_ASSERT_FALSE(result_code);
 }
 
 static void handles_valid_user_with_invalid_identity_token() {
-	CU_PASS("Implement Later");
+	char buffer[2048];
+
+    int result_code = get_access_token(buffer, strlen(test_user_name), test_user_name, fake_identity_token);
+
+    CU_ASSERT_FALSE(result_code);
 }
 
-static void handles_invalid_user_with_invalid_identity_token() {
-	CU_PASS("Implement Later");
+static void handles_invalid_user_with_valid_identity_token() {
+	char buffer[2048];
+
+    int result_code = get_access_token(buffer, strlen(fake_user_name), fake_user_name, test_identity_token);
+
+    CU_ASSERT_FALSE(result_code);
 }
 
 static CU_TestInfo test_get_auth[] = {
-	{"handles_valid_users_authenticating", handles_valid_user},
-	{"handles_invalid_users_correctly", handles_invalid_user},
+	{"handles valid users authenticating", handles_valid_user},
+	{"handles invalid users correctly", handles_invalid_user},
+    {"handles a valid user without a correct ID token", handles_valid_user_with_invalid_identity_token},
+    {"handles an invalid user with a correct ID token", handles_invalid_user_with_valid_identity_token},
 	CU_TEST_INFO_NULL
 };
 
-static CU_TestInfo test_unknown_identity_token[] = {
-	CU_TEST_INFO_NULL
-};
 
 
 //{suite name, init func, cleanup func, setup func, teardown func}
 static CU_SuiteInfo suites[] = {
-	{"gets_access_token", NULL, NULL, NULL, NULL, test_get_auth},
+	{"handles retreviing access token", NULL, NULL, NULL, NULL, test_get_auth},
 	CU_SUITE_INFO_NULL
 };
 

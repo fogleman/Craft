a9246a7b709a0bfc018f02e78d8b98e88c4791ce
strozzi1 <strozzij@oregonstate.edu>
2019-04-29 21:37:57 -0700

    begun writting tests for items

---
+++ test/item_test.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+//#include <math.h>
+
+#include "../src/item.h"
+
+#include <CUnit/CUnit.h>
+#include "item_test.h"
+
+static void handleValidTransparent(){
+
+}
+
+static void handleInvalidTransparent(){
+
+
+}
+
+static void handleValidObstacle(){
+
+
+}
+
+static void handleInvalidObstacle(){
+
+    
+}
+
+static void handleValidDestructable(){
+
+
+}
+
+static void handleInvalidDestructable(){
+
+
+}
+
+static void handleValidPlant(){
+
+
+}
+
+static void handleInvalidPlant(){
+
+
+
+}

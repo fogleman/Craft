6955a1205dc95e4f3c58394f242a28a7e6e3d405
strozzi1 <strozzij@oregonstate.edu>
2019-04-30 00:33:29 -0700

    uncommented out ItemTest in craft_test.c

--- test/craft_test.c
+++ test/craft_test.c
@@ -11,7 +11,7 @@ void AddAllTests() {
 	TestTest_AddTests();
 	AuthTest_AddTests();
     MatrixTest_AddTests();
-	//ItemTest_AddTests();
+	ItemTest_AddTests();
 }
 
 int main(int argc, char** argv) {

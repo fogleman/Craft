b9abb95218641101b861b897b58da70c3be697a8
strozzij <strozzij@oregonstate.edu>
2019-04-30 14:27:09 -0700

    tests are written and done

--- test/item_test.c
+++ test/item_test.c
@@ -17,8 +17,9 @@ static void handleValidTransparent(){
 
 static void handleInvalidTransparent(){
     for (int i = 1; i<64; i++){                
-        if(i!=10 && i!=15 && i!=17 && i!=19 && i!=20 && i!=21 && i!=22  && i!=23 ) //apparently plants are transparent
-            CU_ASSERT_FALSE(is_transparent(i));           
+        if(i!=10 && i!=15 && i!=17 && i!=18 && i!=19 && i!=20 && i!=21 && i!=22 && i!=23 ) //apparently plants are transparent
+            CU_ASSERT_FALSE(is_transparent(i));     
+   
     }
 
 }
@@ -70,32 +71,32 @@ static CU_TestInfo transparency_tests[] = {
     {"Properly handles valid transparent items", handleValidTransparent},
     {"Properly handles invalid transparent items", handleInvalidTransparent},
     CU_TEST_INFO_NULL
-}
+};
 
 static CU_TestInfo destructable_tests[] = {
     {"Properly handles valid destructable items", handleValidDestructable},
     {"Properly handles invalid destructable items", handleInvalidDestructable},
     CU_TEST_INFO_NULL
-}
+};
 
 static CU_TestInfo obstacle_tests[] = {
     {"Properly handles valid obstacle items", handleValidObstacle},
     {"Properly handles invalid obstacle items", handleInvalidObstacle},
     CU_TEST_INFO_NULL
-}
+};
 
 static CU_TestInfo plant_tests[] = {
     {"Properly handles valid plant items", handleValidPlant},
     {"Properly handles invalid plant items", handleInvalidPlant},
     CU_TEST_INFO_NULL
 
-}
+};
 
 static CU_SuiteInfo suites[] = {
     {"transparency suite", NULL, NULL, NULL, NULL, transparency_tests},
     {"obstacle suite", NULL, NULL, NULL, NULL, obstacle_tests},
     {"destructable suite", NULL, NULL, NULL, NULL, destructable_tests},
-    {"plant suite", NULL, NULL, NULL, NULL, plan_tests},
+    {"plant suite", NULL, NULL, NULL, NULL, plant_tests},
     CU_SUITE_INFO_NULL
 };
 

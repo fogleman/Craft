026c5453a2ae396326d95e1465921671d94c7f4c
strozzi1 <strozzij@oregonstate.edu>
2019-04-29 22:24:58 -0700

    testing suites set up, just need tests

--- test/item_test.c
+++ test/item_test.c
@@ -9,6 +9,7 @@
 #include "item_test.h"
 
 static void handleValidTransparent(){
+    
 
 }
 
@@ -46,4 +47,48 @@ static void handleInvalidPlant(){
 
 
 
+}
+
+
+static CU_TestInfo transparency_tests[] = {
+    {"Properly handles valid transparent items", handleValidTransparent},
+    {"Properly handles invalid transparent items", handleInvalidTransparent},
+    CU_TEST_INFO_NULL
+}
+
+static CU_TestInfo destructable_tests[] = {
+    {"Properly handles valid destructable items", handleValidDestructable},
+    {"Properly handles invalid destructable items", handleInvalidDestructable},
+    CU_TEST_INFO_NULL
+}
+
+static CU_TestInfo obstacle_tests[] = {
+    {"Properly handles valid obstacle items", handleValidObstacle},
+    {"Properly handles invalid obstacle items", handleInvalidObstacle},
+    CU_TEST_INFO_NULL
+}
+
+static CU_TestInfo plant_tests[] = {
+    {"Properly handles valid plant items", handleValidPlant},
+    {"Properly handles invalid plant items", handleInvalidPlant},
+    CU_TEST_INFO_NULL
+
+}
+
+static CU_SuiteInfo suites[] = {
+    {"transparency suite", NULL, NULL, NULL, NULL, transparency_tests},
+    {"obstacle suite", NULL, NULL, NULL, NULL, obstacle_tests},
+    {"destructable suite", NULL, NULL, NULL, NULL, destructable_tests},
+    {"plant suite", NULL, NULL, NULL, NULL, plan_tests},
+    CU_SUITE_INFO_NULL
+};
+
+void ItemTest_AddTests() {
+    assert(NULL != CU_get_registry());
+    assert(!CU_is_test_running());
+
+    if(CU_register_suites(suites) != CUE_SUCCESS) {
+        fprintf(stderr, "suite registration failed - %s\n", CU_get_error_msg());
+        exit(EXIT_FAILURE);
+    }
 }

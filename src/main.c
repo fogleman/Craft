42523a1cd7e67891514becd63392d131b21b6e70
kwinthrop <kwinthrop2016@my.fit.edu>
2019-12-03 19:47:36 -0500

    removed test print

--- src/main.c
+++ src/main.c
@@ -2874,7 +2874,7 @@ int main(int argc, char **argv) {
                     item_names[g->item_index], chunked(s->x), chunked(s->z), s->x, s->y, s->z,
                     g->player_count, g->chunk_count,
                     face_count * 2, hour, am_pm, fps.fps);
-                snprintf(text_buffer, 50, "TEST x:%.2f y:%.2f z:%.2f", s->x, s->y, s->z);
+                //snprintf(text_buffer, 50, "TEST x:%.2f y:%.2f z:%.2f", s->x, s->y, s->z);
                 render_text(&text_attrib, ALIGN_LEFT, tx, ty, ts, text_buffer);
                 ty -= ts * 2;
             }

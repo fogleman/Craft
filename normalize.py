af88d6462c069143d6dc8e1a6ff5df69e33e6061
iNateDawg <43409988+iNateDawg@users.noreply.github.com>
2020-03-16 15:59:49 -0400

    Update normalize.py
    Pixel value array has been quadrupled in size to create a larger map, 1px now corresponds to a 2x2 section of blocks on the map . Also updated some logic to the max and minimum values from the normalized pixel array so the minimum array value is no longer 0 but is now 12, and the max value of the array isn't the max block height limit.

--- normalize.py
+++ normalize.py
@@ -27,21 +27,31 @@ class NormArray:
 #Normalize means taking an array of value and making min 0 and max 100 (or whatever value you want to the max)
 #Returns normalized array
 def normalize(image):
-    #scan image, resize to equal dimensions, convert to array of rgb values.
+     # scan image, resize to equal dimensions, convert to array of rgb values.
+    height = 1000; width = 1000; max_block_height = 100
     img = plt.imread(image)
-    img = cv2.resize(img, (1000, 1000))
+    img = cv2.resize(img, (height, width))
     img_arr = np.asarray(img)
-    norm_arr = NormArray(1000, 1000)
+    norm_arr = NormArray(height*2, width*2)
 
-    #get max and min values from img_arr
+    # get max and min values from img_arr
     max = img_arr.max(); min = img_arr.min()
 
-    #copy img_arr to norm_arr
+    p = 0; q= 0
+    # copy img_arr to norm_arr
     for x in range(norm_arr.height):
+        if (x % 2) == 0 and x != 0 and p < height:
+            p += 1
         for y in range(norm_arr.width):
-            val = img_arr[x][y].min()
-            val = ((val - min)/(max - min)) * 256   #depth of blocks is from 0 to 256 blocks.
-            val = int(val)
+            if(y % 2) == 0 and y != 0 and q < height:
+                q += 1
+            val = img_arr[p][q].min()
+            # depth of blocks is from 0 to 256 blocks, but max height will be 100 to give room to build higher
+            # and have a more terrain that's easier to traverse
+            val = ((val - min) / (max - min)) * max_block_height
+            #added 12 b/c the lowest point in the image should still be able to be mined past
+            val = int(val + 12)
             norm_arr.setval(x, y, val)
+        q = 0
 
     return norm_arr

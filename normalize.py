55862528ea616dc214698bf9e0bf34cb43317020
iNateDawg <43409988+iNateDawg@users.noreply.github.com>
2020-04-08 13:01:28 -0400

    update normalize.py
    Check was added to ensure the image file was a .png.

--- normalize.py
+++ normalize.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 import cv2
 
 
-#Takes height and width as parameters
-#sets and gets values in array
-#returns and array
+# Takes height and width as parameters
+# sets and gets values in array
+# returns and array
 class NormArray:
     def __init__(self, height, width):
         self.height = height
@@ -20,38 +20,60 @@ class NormArray:
         val = self.array[int(x)][int(y)]
         return val
 
-    def getarray (self):
+    def getarray(self):
         return self.array
 
-#Takes image as parameter and normalizes it
-#Normalize means taking an array of value and making min 0 and max 100 (or whatever value you want to the max)
-#Returns normalized array
+
+# Takes image as parameter and normalizes it
+# Normalize means taking an array of value and making min 0 and max 100 (or whatever value you want to the max)
+# Returns normalized array
 def normalize(image):
-     # scan image, resize to equal dimensions, convert to array of rgb values.
-    height = 1000; width = 1000; max_block_height = 100
+    # scan image, resize to equal dimensions, convert to array of rgb values.
+    height = 1000
+    width = 1000
+    max_block_height = 100
     img = plt.imread(image)
     img = cv2.resize(img, (height, width))
     img_arr = np.asarray(img)
-    norm_arr = NormArray(height*2, width*2)
+    norm_arr = NormArray(height * 2, width * 2)
 
     # get max and min values from img_arr
-    max = img_arr.max(); min = img_arr.min()
+    max = img_arr.max()
+    min = img_arr.min()
 
-    p = 0; q= 0
+    p = 0
+    q = 0
     # copy img_arr to norm_arr
     for x in range(norm_arr.height):
         if (x % 2) == 0 and x != 0 and p < height:
             p += 1
         for y in range(norm_arr.width):
-            if(y % 2) == 0 and y != 0 and q < height:
+            if (y % 2) == 0 and y != 0 and q < height:
                 q += 1
             val = img_arr[p][q].min()
             # depth of blocks is from 0 to 256 blocks, but max height will be 100 to give room to build higher
             # and have a more terrain that's easier to traverse
             val = ((val - min) / (max - min)) * max_block_height
-            #added 12 b/c the lowest point in the image should still be able to be mined past
-            val = int(val + 12)
+            # added 12 b/c the lowest point in the image should still be able to be mined past
+            val = int(val + 20)
             norm_arr.setval(x, y, val)
         q = 0
 
     return norm_arr
+
+
+def process_image(image):
+
+    if not image.endswith(".png"):
+        return print("File must be a .png")
+
+    pixel_arr = normalize(image)
+    with open("src/data_file.txt", 'w') as file:
+        for x in range(pixel_arr.height):
+            for y in range(pixel_arr.width):
+                file.write(str(pixel_arr.getval(x, y)) + '\n')
+
+    file.close()
+    return file
+
+#process_image('heightdata.png')

acbda0199b6f67ac1dcf6722cf1ff16c4e242ba8
iNateDawg <43409988+iNateDawg@users.noreply.github.com>
2020-04-19 13:45:57 -0400

    Update world.c
    This update fixes the issues in pull request #25

--- src/world.c
+++ src/world.c
@@ -23,7 +23,7 @@ void create_world(int p, int q, world_func func, int pixelArray[][2000], void *a
             float g = simplex2(-x * 0.01, -z * 0.01, 2, 0.9, 2);
             int mh = g * 32 + 16;
             int h;
-            if(x > 0 && x < 2000 && z < 0 && z < 2000){
+            if(x > 0 && x < 2000 && z > 0 && z < 2000){
                 h = pixelArray[x][z];
             }
             else{

163e819b837170f145a01863a087ac85ac1f5df9
Ubuntu <hemmelgarn.82@wright.edu>
2020-05-01 13:47:06 -0700

    Jump minigame

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 #include <time.h>
 #include "auth.h"
@@ -41,6 +42,8 @@
 
 static int spee = 5;
 static int jumpHeight = 8;
+static int jumpCount = 0;
+static bool jumpCheck = false;
 
 typedef struct {
     Map map;
@@ -2029,43 +2032,24 @@ void parse_command(const char *buffer, int forward) {
     char filename[MAX_PATH_LENGTH];
     int radius, count, xc, yc, zc;
     int amount;
-    int defJump = 8;
+    int none;
     if (sscanf(buffer, "/identity %128s %128s", username, token) == 2) {
         db_auth_set(username, token);
         add_message("Successfully imported identity token!");
         login();
     }
-    else if (strcmp(buffer, "/logout") == 0) {
-        db_auth_select_none();
-        login();
+    else if (sscanf(buffer, "/setSpeed %d", &amount) == 1) {
+        spee = amount;
     }
-    else if (sscanf(buffer, "/login %128s", username) == 1) {
-        if (db_auth_select(username)) {
-            login();
+    else if (sscanf(buffer, "/setJump %d", &amount) == 1) {
+        jumpHeight = amount;
+    }
+    else if (sscanf(buffer, "/jumpGame") == 0) {
+        if(jumpCheck == false) {
+            jumpCheck = true;
+        } else {
+            jumpCheck = false;
         }
-        else {
-            add_message("Unknown username.");
-        }
-    }
-    else if (sscanf(buffer,
-        "/online %128s %d", server_addr, &server_port) >= 1)
-    {
-        g->mode_changed = 1;
-        g->mode = MODE_ONLINE;
-        strncpy(g->server_addr, server_addr, MAX_ADDR_LENGTH);
-        g->server_port = server_port;
-        snprintf(g->db_path, MAX_PATH_LENGTH,
-            "cache.%s.%d.db", g->server_addr, g->server_port);
-    }
-    else if (sscanf(buffer, "/offline %128s", filename) == 1) {
-        g->mode_changed = 1;
-        g->mode = MODE_OFFLINE;
-        snprintf(g->db_path, MAX_PATH_LENGTH, "%s.db", filename);
-    }
-    else if (strcmp(buffer, "/offline") == 0) {
-        g->mode_changed = 1;
-        g->mode = MODE_OFFLINE;
-        snprintf(g->db_path, MAX_PATH_LENGTH, "%s", DB_PATH);
     }
     else if (sscanf(buffer, "/view %d", &radius) == 1) {
         if (radius >= 1 && radius <= 24) {
@@ -2076,72 +2060,6 @@ void parse_command(const char *buffer, int forward) {
         else {
             add_message("Viewing distance must be between 1 and 24.");
         }
-    }
-    else if (strcmp(buffer, "/copy") == 0) {
-        copy();
-    }
-    else if (strcmp(buffer, "/paste") == 0) {
-        paste();
-    }
-    else if (strcmp(buffer, "/tree") == 0) {
-        tree(&g->block0);
-    }
-    else if (sscanf(buffer, "/array %d %d %d", &xc, &yc, &zc) == 3) {
-        array(&g->block1, &g->block0, xc, yc, zc);
-    }
-    else if (sscanf(buffer, "/array %d", &count) == 1) {
-        array(&g->block1, &g->block0, count, count, count);
-    }
-    else if (strcmp(buffer, "/fcube") == 0) {
-        cube(&g->block0, &g->block1, 1);
-    }
-    else if (strcmp(buffer, "/cube") == 0) {
-        cube(&g->block0, &g->block1, 0);
-    }
-    else if (sscanf(buffer, "/fsphere %d", &radius) == 1) {
-        sphere(&g->block0, radius, 1, 0, 0, 0);
-    }
-    else if (sscanf(buffer, "/sphere %d", &radius) == 1) {
-        sphere(&g->block0, radius, 0, 0, 0, 0);
-    }
-    else if (sscanf(buffer, "/fcirclex %d", &radius) == 1) {
-        sphere(&g->block0, radius, 1, 1, 0, 0);
-    }
-    else if (sscanf(buffer, "/circlex %d", &radius) == 1) {
-        sphere(&g->block0, radius, 0, 1, 0, 0);
-    }
-    else if (sscanf(buffer, "/fcircley %d", &radius) == 1) {
-        sphere(&g->block0, radius, 1, 0, 1, 0);
-    }
-    else if (sscanf(buffer, "/circley %d", &radius) == 1) {
-        sphere(&g->block0, radius, 0, 0, 1, 0);
-    }
-    else if (sscanf(buffer, "/fcirclez %d", &radius) == 1) {
-        sphere(&g->block0, radius, 1, 0, 0, 1);
-    }
-    else if (sscanf(buffer, "/circlez %d", &radius) == 1) {
-        sphere(&g->block0, radius, 0, 0, 0, 1);
-    }
-    else if (sscanf(buffer, "/fcylinder %d", &radius) == 1) {
-        cylinder(&g->block0, &g->block1, radius, 1);
-    }
-    else if (sscanf(buffer, "/cylinder %d", &radius) == 1) {
-        cylinder(&g->block0, &g->block1, radius, 0);
-    }
-    else if (sscanf(buffer, "/setSpeed %d", &amount) == 1) {
-        spee = amount;
-    }
-    else if (sscanf(buffer, "/setJump %d", &amount) == 1) {
-        jumpHeight = amount;
-    }
-    else if (sscanf(buffer, "/resSpeed") == 0) {
-        spee = 5;
-    }
-    else if (sscanf(buffer, "/resJump") == 0) {
-        jumpHeight = defJump;
-    }
-    else if (forward) {
-        client_talk(buffer);
     }
 }
 
@@ -2445,14 +2363,16 @@ void handle_movement(double dt) {
     }
     float vx, vy, vz;
     get_motion_vector(g->flying, sz, sx, s->rx, s->ry, &vx, &vy, &vz);
-    int jH = jumpHeight;
     if (!g->typing) {
         if (glfwGetKey(g->window, CRAFT_KEY_JUMP)) {
             if (g->flying) {
                 vy = 1;
             }
-            else if (dy == 0) {
-                dy = jH;
+            else if (dy == 0) {  
+                if(jumpCheck == true) {
+                    jumpHeight++;
+                }
+                dy = jumpHeight;
             }
         }
     }

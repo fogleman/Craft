74f6e363a8a8084ad00e6b73125ddea412de2124
Brandon Eyongherok <brandoneyongherok@Brandons-Air.lan1>
2020-05-02 05:28:06 -0400

    increase the screen width

--- src/config.h
+++ src/config.h
@@ -4,7 +4,7 @@
 // app parameters
 #define DEBUG 0
 #define FULLSCREEN 0
-#define WINDOW_WIDTH 1024
+#define WINDOW_WIDTH 1099
 #define WINDOW_HEIGHT 768
 #define VSYNC 1
 #define SCROLL_THRESHOLD 0.1

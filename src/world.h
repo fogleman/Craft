7ac080315dc73eb8b5b1fd7ad2c3290cae7bda58
Alex Reigle <71418886+alexreigle@users.noreply.github.com>
2021-04-05 17:50:52 -0400

    Update world.h

--- src/world.h
+++ src/world.h
@@ -1,8 +1,6 @@
 #ifndef _world_h_
 #define _world_h_
 
-// #include <time.h>
-
 typedef void (*world_func)(int, int, int, int, void *);
 
 void create_world(int p, int q, world_func func, void *arg);

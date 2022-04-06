a56c3dc3de3790d81734d6177ac6966ad6761446
Izak Halseide <ihalseide@pm.me>
2022-04-06 14:09:00 -0600

    Add comments to world.c and world.h

--- src/world.c
+++ src/world.c
@@ -2,30 +2,40 @@
 #include "noise.h"
 #include "world.h"
 
+// Main terrain generation function
+// Parameters:
+// - p: chunk p location
+// - q: chunk q location
+// - func: function callback to modify blocks in the world (see world.h)
+// - arg: last argument to be used for the function callback
 void create_world(int p, int q, world_func func, void *arg) {
+	// GUESS: the inclusion of the extra pad locations is for tree generation across chunk borders.
     int pad = 1;
+	// Loop for each (x, z) location in chunk (p, q):
     for (int dx = -pad; dx < CHUNK_SIZE + pad; dx++) {
         for (int dz = -pad; dz < CHUNK_SIZE + pad; dz++) {
             int flag = 1;
             if (dx < 0 || dz < 0 || dx >= CHUNK_SIZE || dz >= CHUNK_SIZE) {
                 flag = -1;
             }
-            int x = p * CHUNK_SIZE + dx;
-            int z = q * CHUNK_SIZE + dz;
+            int x = p * CHUNK_SIZE + dx; // convert p (chunk x) and dx to world x
+            int z = q * CHUNK_SIZE + dz; // convert q (chunk z) and dz to world z
             float f = simplex2(x * 0.01, z * 0.01, 4, 0.5, 2);
             float g = simplex2(-x * 0.01, -z * 0.01, 2, 0.9, 2);
             int mh = g * 32 + 16;
             int h = f * mh;
-            int w = 1;
+			// w = block id
+            int w = 1; // grass
             int t = 12;
             if (h <= t) {
                 h = t;
-                w = 2;
+                w = 2; // sand
             }
             // sand and grass terrain
             for (int y = 0; y < h; y++) {
                 func(x, y, z, w * flag, arg);
             }
+			// Place plants on grass (block id 1).
             if (w == 1) {
                 if (SHOW_PLANTS) {
                     // grass
@@ -68,7 +78,7 @@ void create_world(int p, int q, world_func func, void *arg) {
                     if (simplex3(
                         x * 0.01, y * 0.1, z * 0.01, 8, 0.5, 2) > 0.75)
                     {
-                        func(x, y, z, 16 * flag, arg);
+                        func(x, y, z, 16 * flag, arg); // block id 16 is cloud
                     }
                 }
             }
--- src/world.h
+++ src/world.h
@@ -1,7 +1,8 @@
 #ifndef _world_h_
 #define _world_h_
 
-typedef void (*world_func)(int, int, int, int, void *);
+// World function callback (apparently used to modify a world's blocks)
+typedef void (*world_func)(int x, int y, int z, int w, void *arg);
 
 void create_world(int p, int q, world_func func, void *arg);
 

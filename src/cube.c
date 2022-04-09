2df1f4c8748a9a91edc79e320abf11ab826797e7
Izak Halseide <ihalseide@pm.me>
2022-04-09 17:37:52 -0600

    Start adding comments to cube.c

--- src/cube.c
+++ src/cube.c
@@ -4,12 +4,36 @@
 #include "matrix.h"
 #include "util.h"
 
+// Make a complete cube model.
+// Arguments:
+// - data: output pointer
+// - ao: TODO what exactly is ao?
+// - light: TODO what exactly is light?
+// - left: whether to generate a left face
+// - right: whether to generate a right face
+// - top: whether to generate a top face
+// - bottom: whether to generate a bottom face
+// - front: whether to generate a front face
+// - back: whether to generate a back face
+// - wleft: texture tile number for left face
+// - wright: texture tile number for right face
+// - wtop: texture tile number for top face
+// - wbottom: texture tile number for bottom face
+// - wfront: texture tile number for front face
+// - wback: texture tile number for back face
+// - x: block x center position
+// - y: block y center position
+// - z: block z center position
+// - n: cube scale; the distance of each face from the cube center
+// Returns:
+// - writes specific values to the data pointer
 void make_cube_faces(
     float *data, float ao[6][4], float light[6][4],
     int left, int right, int top, int bottom, int front, int back,
     int wleft, int wright, int wtop, int wbottom, int wfront, int wback,
     float x, float y, float z, float n)
 {
+	// 6 faces each with 4 points, each of which are 3-vectors
     static const float positions[6][4][3] = {
         {{-1, -1, -1}, {-1, -1, +1}, {-1, +1, -1}, {-1, +1, +1}},
         {{+1, -1, -1}, {+1, -1, +1}, {+1, +1, -1}, {+1, +1, +1}},
@@ -18,6 +42,7 @@ void make_cube_faces(
         {{-1, -1, -1}, {-1, +1, -1}, {+1, -1, -1}, {+1, +1, -1}},
         {{-1, -1, +1}, {-1, +1, +1}, {+1, -1, +1}, {+1, +1, +1}}
     };
+	// 6 faces each with a 3-vector normal direction
     static const float normals[6][3] = {
         {-1, 0, 0},
         {+1, 0, 0},
@@ -26,6 +51,7 @@ void make_cube_faces(
         {0, 0, -1},
         {0, 0, +1}
     };
+	// 6 faces each with 4 points, each of which are 2-vectors
     static const float uvs[6][4][2] = {
         {{0, 0}, {1, 0}, {0, 1}, {1, 1}},
         {{1, 0}, {0, 0}, {1, 1}, {0, 1}},
@@ -42,6 +68,7 @@ void make_cube_faces(
         {0, 3, 2, 0, 1, 3},
         {0, 3, 1, 0, 2, 3}
     };
+	// flipped indices
     static const float flipped[6][6] = {
         {0, 1, 2, 1, 3, 2},
         {0, 2, 1, 2, 3, 1},
@@ -57,35 +84,61 @@ void make_cube_faces(
     int faces[6] = {left, right, top, bottom, front, back};
     int tiles[6] = {wleft, wright, wtop, wbottom, wfront, wback};
     for (int i = 0; i < 6; i++) {
-		// Do not generate faces which were not specified.
+		// i is an index into the faces
+		// Do not write to faces which were not specified.
         if (faces[i] == 0) {
             continue;
         }
+		// Convert texture tile number to texture pixel coordinates
         float du = (tiles[i] % 16) * s;
         float dv = (tiles[i] / 16) * s;
         int flip = ao[i][0] + ao[i][3] > ao[i][1] + ao[i][2];
         for (int v = 0; v < 6; v++) {
+			// j is index into the points
             int j = flip ? flipped[i][v] : indices[i][v];
+			// Write the position 3-vector
             *(d++) = x + n * positions[i][j][0];
             *(d++) = y + n * positions[i][j][1];
             *(d++) = z + n * positions[i][j][2];
+			// Write the normal 3-vector
             *(d++) = normals[i][0];
             *(d++) = normals[i][1];
             *(d++) = normals[i][2];
+			// Write the texture UV 2-vector
             *(d++) = du + (uvs[i][j][0] ? b : a);
             *(d++) = dv + (uvs[i][j][1] ? b : a);
+			// TODO: comment what the next line does
             *(d++) = ao[i][j];
+			// TODO: comment what the next line does
             *(d++) = light[i][j];
         }
     }
 }
 
+// Make a cube model for a block
+// Arguments:
+// - data: output pointer
+// - ao: TODO what exactly is ao?
+// - light: TODO what exactly is light?
+// - left: whether to generate a left face
+// - right: whether to generate a right face
+// - top: whether to generate a top face
+// - bottom: whether to generate a bottom face
+// - front: whether to generate a front face
+// - back: whether to generate a back face
+// - x: block x center position
+// - y: block y center position
+// - z: block z center position
+// - n: cube scale; the distance of each face from the cube center
+// - w: block id to use for textures
+// Returns:
+// - writes specific values to the data pointer
 void make_cube(
     float *data, float ao[6][4], float light[6][4],
     int left, int right, int top, int bottom, int front, int back,
     float x, float y, float z, float n, int w)
 {
-	// Get blocks texture faces (blocks array is defined in item.c)
+	// Get blocks texture faces (the blocks lookup table is defined in item.c)
     int wleft = blocks[w][0];
     int wright = blocks[w][1];
     int wtop = blocks[w][2];
@@ -99,6 +152,19 @@ void make_cube(
         x, y, z, n);
 }
 
+// Make a plant model
+// Arguments:
+// - data: output pointer
+// - ao: TODO what exactly is ao?
+// - light: TODO what exactly is light?
+// - px:
+// - py:
+// - pz:
+// - n:
+// - w:
+// - rotation:
+// Returns:
+// - writes specific values to the data pointer
 void make_plant(
     float *data, float ao, float light,
     float px, float py, float pz, float n, int w, float rotation)
@@ -131,20 +197,26 @@ void make_plant(
     float s = 0.0625;
     float a = 0;
     float b = s;
+	// Convert texture tile number to texture pixel coordinates
     float du = (plants[w] % 16) * s;
     float dv = (plants[w] / 16) * s;
     for (int i = 0; i < 4; i++) {
         for (int v = 0; v < 6; v++) {
             int j = indices[i][v];
+			// Write the position 3-vector
             *(d++) = n * positions[i][j][0];
             *(d++) = n * positions[i][j][1];
             *(d++) = n * positions[i][j][2];
+			// Write the normal 3-vector
             *(d++) = normals[i][0];
             *(d++) = normals[i][1];
             *(d++) = normals[i][2];
+			// Write the UV 2-vector
             *(d++) = du + (uvs[i][j][0] ? b : a);
             *(d++) = dv + (uvs[i][j][1] ? b : a);
+			// Write the ao value
             *(d++) = ao;
+			// Write the light value
             *(d++) = light;
         }
     }

f615798f9a62a996a8e1753384e69c4b65e9699b
Biran <birayaha@3c063041fedb.ant.amazon.com>
2022-12-20 22:08:56 -0800

    disable auth

--- server.py
+++ server.py
@@ -14,6 +14,7 @@ import traceback
 import os
 import psycopg2
 import signal
+from datetime import datetime as dt
 
 cmd = 'rm -rf /tmp/healthy'
 user=os.environ['PGUSER']
@@ -405,7 +406,7 @@ class Model(object):
             client.send(REDRAW, p, q)
             client.send(TALK, message)
             return
-        now=dt.now()
+        now = datetime.datetime.utcnow()
         if RECORD_HISTORY:
             sql = """insert into block_history (timestamp, user_id, x, y, z, w) values (%s,%s,%s,%s,%s,%s)"""
             params=[now,client.user_id,x,y,z,w]

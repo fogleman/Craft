7bd01aadd2255a8272fa5b272efde8d66ba8626c
Biran <birayaha@3c063041fedb.ant.amazon.com>
2022-12-29 15:25:07 -0800

    agones_13

--- server.py
+++ server.py
@@ -240,6 +240,8 @@ class Handler(socketserver.BaseRequestHandler):
             url='http://localhost:'+AGONES_SDK_HTTP_PORT+'/health'
             r=requests.post(url,headers=headers,json={})
             #log('in Handler:run:response-agones:url:',url, ' response.status_code:',r.status_code,' response.headers:',r.headers)
+          else:
+            break
           time.sleep(10)
         except Exception as error:
           log('agones_health:error',error)
